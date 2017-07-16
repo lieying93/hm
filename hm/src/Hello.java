552d54e8e671a8f859d69e12b03d3803abd5f235
lieying93 <750581272@qq.com>
2017-07-16 10:43:28 +0800

    推送到github上

--- hm/src/Hello.java
+++ hm/src/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Hello world  dsafsafdas");
-		System.out.println("第三次提交");
+		System.out.println("第三次提交  ");
+		System.out.println("把gbk更改为utf-8");
 	}
 
 }
